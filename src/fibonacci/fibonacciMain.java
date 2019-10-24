0e25bdeb968ab4ca06dbceb010bacad02991d607
Jeschusmorio <mpetrovic1@tsn.at>
2019-10-24 08:10:33 +0200

    added input

--- src/fibonacci/fibonacciMain.java
+++ src/fibonacci/fibonacciMain.java
@@ -1,15 +1,37 @@
 package fibonacci;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class fibonacciMain {
 
 	public static void main(String[] args) {
-		for (int i = 0; i <= 20; i++) {
-			int limit = i;
-			int fibonacci = fibonacci(1, 0, limit, 2);
-			System.out.println("Fibonacci-Folge bis Grenze "+limit+" = "+ fibonacci);
-		}
+		int limit = input();;
+		int fibonacci = fibonacci(1, 0, limit, 2);
+		System.out.println("Fibonacci-Folge bis Grenze "+limit+" = "+ fibonacci);
 	}
-
+	
+	public static int input() {
+		int input = 0;
+		boolean inputOk;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Geben Sie eine Grenze N ein: ");
+			try {
+				input = sc.nextInt();
+				inputOk = true;
+				if (input < 0) {
+					System.out.println("Die Zahl muss positiv sein!");
+					inputOk = false;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Sie müssen eine Zahl eingeben!");
+				inputOk = false;
+			}
+		} while (!inputOk);
+		return input;
+		
+	}
 	public static int fibonacci(int lastNumber, int numberBeforeLastNumber, int limit, int counter) {
 		if (limit == 0) {
 			return 0;
